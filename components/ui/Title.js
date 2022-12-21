0f925ffe0aeafabe2e1a61cab1cae4f3b360371e
bange-git <sebastienbange@gmail.com>
2022-12-21 06:04:44 +0100

    using different styles based on platforms ie ios or android

--- components/ui/Title.js
+++ components/ui/Title.js
@@ -1,4 +1,4 @@
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, Platform } from "react-native";
 
 const Title = ({ children }) => {
   return <Text style={styles.title}>{children}</Text>;
@@ -10,7 +10,7 @@ const styles = StyleSheet.create({
     fontFamily: 'open-sans-bold',
     color: 'white',
     textAlign: "center",
-    borderWidth: 2,
+    borderWidth: Platform.select({ios: 0, android: 2}),
     padding: 12,
     borderColor: 'white',
   },
